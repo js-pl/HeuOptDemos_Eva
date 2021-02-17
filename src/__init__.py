8295d828b3b04b4ee1e327fea123f2ea7f8aa46f
Eva Gradwohl <evagradwohl@gmx.at>
2021-02-17 12:05:50 +0100

    added creation of log folders

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
+from genericpath import exists
 import os
 import sys
 path = os.getcwd()
 sys.path.append(path + os.path.sep + 'pymhlib_fork_copy')
-
+os.makedirs('logs' + os.path.sep + 'saved', exist_ok=True)
+os.makedirs('logs' + os.path.sep + 'saved_runtime', exist_ok=True)
